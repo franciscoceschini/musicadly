cc1bcbbb7edff4463f49322d0d7e6f4902abd329
javier-16 <javiercogorno16@gmail.com>
2025-10-24 01:51:38 -0300

    prueba

--- src/Musicadly.java
+++ src/Musicadly.java
@@ -5,7 +5,9 @@ public class Musicadly {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("creadores: francico, javier y dante")
         System.out.println("🎵 ¡Bienvenido a Musicadly! 🎵");
+        System.out.println("¡disfruten este juego!");
         System.out.print("Elegí un artista: ");
         String artistName = scanner.nextLine();
 
